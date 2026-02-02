d0c47199dff32f5f6ea62ecfcb44715ef5726987
Satyannarayana Yedla <syysatya@gmail.com>
2026-02-02 17:36:57 +0530

    Add SumProduct program to calculate sum and product
    This program calculates the sum and product of two integers.

---
+++ Variables/SumProduct.cpp
@@ -0,0 +1,20 @@
+/*
+Program Name : SumProduct.cpp
+Concept      : Variables & Arithmetic
+Input        : Two integers
+Output       : Sum and Product
+Logic        : Use variables to store values and compute results
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b = 4;
+    int sum = a + b;
+    int product = a * b;
+
+    cout << "Sum = " << sum << endl;
+    cout << "Product = " << product << endl;
+
+    return 0;
+}
