350793c4c5b18b605cee5ffaae6c67759c1509db
Satyannarayana Yedla <syysatya@gmail.com>
2026-02-02 17:35:57 +0530

    Add VariableDemo.cpp to demonstrate variable usage
    This program demonstrates variable declaration and output in C++. It initializes and displays an integer, a float, and a character.

---
+++ Variables/VariableDemo.cpp
@@ -0,0 +1,21 @@
+/*
+Program Name : VariableDemo.cpp
+Concept      : Variables
+Input        : None
+Output       : Displays variable values
+Logic        : Declare variables, assign values, print them
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 16;
+    float height = 5.8;
+    char grade = 'A';
+
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+
+    return 0;
+}
