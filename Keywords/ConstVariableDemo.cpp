3af553e21265af82715e2f0bae4b33fbf94e4132
Satyannarayana Yedla <syysatya@gmail.com>
2026-02-03 16:46:08 +0530

    Add ConstVariableDemo to demonstrate const keyword
    This program demonstrates the use of the const keyword by defining a constant integer and printing its value.

---
+++ Keywords/ConstVariableDemo.cpp
@@ -0,0 +1,16 @@
+/*
+Program Name : ConstVariableDemo
+Concept      : const keyword
+Input        : None
+Output       : Displays constant value
+Logic        : Define a constant and print it
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int MAX = 100;
+    cout << "Maximum value is: " << MAX << endl;
+    return 0;
+}
