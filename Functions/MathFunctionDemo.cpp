1425a3dbf3c159a1d683b7a06b267ef783e14a9e
Satyannarayana Yedla <syysatya@gmail.com>
2026-02-10 17:11:12 +0530

    Add MathFunctionDemo.cpp for sqrt and pow examples
    Demonstrate function calls for square root and power calculations.

---
+++ Functions/MathFunctionDemo.cpp
@@ -0,0 +1,24 @@
+/*
+Program Name : MathFunctionDemo.cpp
+Concept      : Function calls
+Input        : None
+Output       : Square root and power
+Logic        : Use sqrt() and pow()
+*/
+
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+
+    string name;
+
+    cout << "The square root of 49 is: " <<  sqrt(49) << endl;
+
+    cout << "The 2 to the power 5 is:  " << pow(2,5) << endl;
+
+
+    return 0;
+}
+
