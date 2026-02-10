54a6d66097e094a92042994330ec53b64f1b502e
Satyannarayana Yedla <syysatya@gmail.com>
2026-02-10 16:37:37 +0530

    Add SquareRootDemo program using sqrt function
    This program demonstrates the use of the sqrt() function from cmath to calculate the square root of 16.

---
+++ Functions/sqrtdemo.cpp
@@ -0,0 +1,18 @@
+/*
+Program Name : SquareRootDemo.cpp
+Concept      : Using standard function
+Input        : None
+Output       : Square root of 16
+Logic        : Use sqrt() from cmath
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    cout << "Square root of 16 is " << sqrt(16);
+    return 0;
+}
+
